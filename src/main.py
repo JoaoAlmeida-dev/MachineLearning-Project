6965fbcede7f982c9350241fc362ccb9d41a87ea
joao <joao.ranger@gmail.com>
2022-01-02 16:43:58 +0000

    Random Removal

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from typing import List
 
-from src.Loader import CsvLoader
+from src.dataTreatment.RandomRemoval import RandomRemoval
+from src.loader import CsvLoader
 from src.models.Wine import Wine
 
 RED_CSV = 'Resources/winequality-red.csv'
@@ -8,9 +9,10 @@ WHITE_CSV = 'Resources/winequality-white.csv'
 
 
 def main():
-    wine_list: List[Wine] = CsvLoader.load('%s' % RED_CSV,skip_header=True)
+    wine_list: List[Wine] = CsvLoader.load('%s' % RED_CSV, skip_header=True)
     print(wine_list)
-
-
+    print("len=",len(wine_list))
+    RandomRemoval(dataset=wine_list,removal_percentage=0.1)
+    print("len=",len(wine_list))
 if __name__ == '__main__':
     main()
