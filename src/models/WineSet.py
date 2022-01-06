2d306ce76645bff82635dbc006eff21cc8fe6151
SoPlays <joao.ranger@gmail.com>
2022-01-06 17:36:19 +0000

    WineSet is now Subscriptable and Iterable

--- src/models/WineSet.py
+++ src/models/WineSet.py
@@ -8,13 +8,27 @@ from src.dataTreatment.WineTranspose import wine_list_Transpose
 from src.models.Wine import Wine
 
 
+class WineSetIterator:
+    def __init__(self, wineSet):
+        self._wineSet = wineSet
+        self._index = 0
+
+    def __next__(self):
+        if self._index < len(self._wineSet) :
+            result = self._wineSet[self._index]
+            self._index += 1
+            return result
+        else:
+            # End of Iteration
+            raise StopIteration
+
 class WineSet:
 
     def __init__(self, wine_list: List[Wine] = None, wine_dataframe: DataFrame = None):
         if wine_list is not None:
             self.wine_list = wine_list
-            self._len = len(wine_list)
             self.transposed = wine_list_Transpose(wine_list)
+
             NDarray = []
             features_asNDArray = []
             labels_asNDArray = []
@@ -22,6 +36,7 @@ class WineSet:
                 NDarray.append(wine.asNDArray)
                 features_asNDArray.append(wine.features_asNDArray)
                 labels_asNDArray.append(wine.labels_asNDArray)
+
             self._asNDarray:ndarray = numpy.asarray(NDarray)
             self._features_asNDArray:ndarray = numpy.asarray(features_asNDArray)
             self._labels_asNDArray:ndarray = numpy.asarray(labels_asNDArray)
@@ -31,7 +46,13 @@ class WineSet:
             pass
 
     def __len__(self):
-        return self._len
+        return len(self.wine_list)
+
+    def __iter__(self):
+        return WineSetIterator(self)
+
+    def __getitem__(self, item):
+        return self.wine_list[item]
 
     @property
     def features_asNDArray(self):
@@ -44,3 +65,5 @@ class WineSet:
     @property
     def asNDarray(self):
         return self._asNDarray
+
+
