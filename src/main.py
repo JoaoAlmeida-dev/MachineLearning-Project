23ba3b43c78136f6a3c9a70a6ec59af0c3eee8fd
SoPlays <joao.ranger@gmail.com>
2022-01-13 06:20:41 +0000

    general

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import copy
+import os
 import random
 from typing import List
 
@@ -21,7 +22,7 @@ from src.algorithms.unsupervised.DBScanClass import DBScanClass
 from src.algorithms.unsupervised.KMeansClass import KMeansClass
 from src.constants.Constants import RED_CSV, WHITE_CSV, MIN_MAX, CORRELATIONS, TARGET
 from src.dataTreatment.DataDiscretization import discretize
-from src.dataTreatment.DataNormalization import normalize_set_log, normalize_set_range, normalize_set_mean
+from src.dataTreatment.DataNormalization import normalize_set_log, normalize_set_range, normalize_standard_scaler
 from src.dataTreatment.DataReduction import reduce
 from src.helper.Correlation import correlate
 from src.helper.RandomRemoval import random_removal_mean
@@ -29,12 +30,12 @@ from src.loader import CsvLoader
 # from src.models.WineSet import WineSet
 from src.plotting.WinePlot import plot_hist_wine_set, plot_wine_set, plot_decision_regions_algo, plot_means_red_white
 
-ALGO = False
-PLOT = True
-NORMALIZE = False
-DISCRETIZE = False
-REDUCE = False
-REMOVE = False
+ALGO = True
+PLOT = False
+NORMALIZE = True
+DISCRETIZE = True
+REDUCE = True
+REMOVE = True
 ORIGINAL = True
 
 
@@ -50,31 +51,54 @@ def run_algos(wine_set: DataFrame, title: str):
     labels_train = labels_train.ravel()
 
     # region Supervised
-    knn: KNeighborsClassifier = KNNClass.run(features_train, features_test, labels_train, labels_test)
-    plot_decision_regions_algo(wine_set=wine_set,wine_set_title=title, algorithm=knn, algorithm_name="KNN")
+    knn, knn_accuracy = KNNClass.run(features_train, features_test, labels_train, labels_test)
+    decisionTree, decisionTree_accuracy = DecisionTreeClass.run(features_train, features_test, labels_train,
+                                                                labels_test)
+    mlp, mlp_accuracy = MultiLayerPercetronClass.run(features_train, features_test, labels_train, labels_test)
 
-    decisionTree: DecisionTreeClassifier = DecisionTreeClass.run(features_train, features_test, labels_train, labels_test)
-    plot_decision_regions_algo(wine_set=wine_set,wine_set_title=title, algorithm=decisionTree, algorithm_name="DecisionTree")
-
-    mlp: MLPClassifier = MultiLayerPercetronClass.run(features_train, features_test, labels_train, labels_test)
-    plot_decision_regions_algo(wine_set=wine_set,wine_set_title=title, algorithm=mlp, algorithm_name="MLPClassifier")
+    if PLOT:
+        plot_decision_regions_algo(wine_set=wine_set, wine_set_title=title, algorithm=knn, algorithm_name="KNN")
+        plot_decision_regions_algo(wine_set=wine_set, wine_set_title=title, algorithm=decisionTree,
+                                   algorithm_name="DecisionTree")
+        plot_decision_regions_algo(wine_set=wine_set, wine_set_title=title, algorithm=mlp,
+                                   algorithm_name="MLPClassifier")
 
     # endregion
     # region Unsupervised
-    agglomerative = AgglomerativeHierarchicalClusteringClass.run(wine_set=wine_set)
-    plot_decision_regions_algo(wine_set=wine_set,wine_set_title=title, algorithm=agglomerative, algorithm_name="Agglomerative")
+    agglomerative, agglomerative_accuracy = AgglomerativeHierarchicalClusteringClass.run(wine_set=wine_set)
+    dbscan, dbscan_accuracy = DBScanClass.run(wine_set=wine_set)
+    kmeans, kmeans_accuracy = KMeansClass.run(wine_set=wine_set)
 
-    dbscan: DBSCAN = DBScanClass.run(wine_set=wine_set)
-    plot_decision_regions_algo(wine_set=wine_set,wine_set_title=title, algorithm=dbscan, algorithm_name="DBSCAN")
-
-    kmeans: KMeans = KMeansClass.run(wine_set=wine_set)
-    plot_decision_regions_algo(wine_set=wine_set,wine_set_title=title, algorithm=kmeans, algorithm_name="Kmeans")
+    if PLOT:
+        plot_decision_regions_algo(wine_set=wine_set, wine_set_title=title, algorithm=agglomerative,
+                                   algorithm_name="Agglomerative")
+        plot_decision_regions_algo(wine_set=wine_set, wine_set_title=title, algorithm=dbscan, algorithm_name="DBSCAN")
+        plot_decision_regions_algo(wine_set=wine_set, wine_set_title=title, algorithm=kmeans, algorithm_name="Kmeans")
     # endregion
 
-    # plot_decision_regions_algo(features_df=features_train,labels_df=labels_train,algorithm= algorithm)
-    #mlxtend.plotting.plot_decision_regions(X=features_train, y=labels_train, clf=decisionTree)
+    #my_path = os.path.join(os.path.abspath(__file__), "..", "plotting", "res", title)
+    #file_name = os.path.join(my_path, title + "_results.txt")
+    #if not os.path.exists(my_path):
+        #os.makedirs(my_path)
+
+    file_name = os.path.join("results.csv")
+    f = open(file_name, "a")
+    # f.write(
+    #    "knn_accuracy," + str(knn_accuracy) + "\n"+
+    #    "decisionTree_accuracy," + str(decisionTree_accuracy) + "\n"+
+    #    "mlp_accuracy," + str(mlp_accuracy) + "\n"+
+    #    "agglomerative_accuracy," + str(agglomerative_accuracy) + "\n"+
+    #    "dbscan_accuracy," + str(dbscan_accuracy) + "\n"+
+    #    "kmeans_accuracy," + str(kmeans_accuracy) + "\n"
+    # )
+    f.write(
+        title +"," + str("%.2f" % knn_accuracy) + "," + str("%.2f" % decisionTree_accuracy) + "," + str("%.2f" % mlp_accuracy) + "," + str(
+            "%.2f" % agglomerative_accuracy) + "," + str("%.2f" % dbscan_accuracy) + "," + str("%.2f" % kmeans_accuracy) + ",\n"
+    )
+    f.close()
 
     print(spacer, "ENDRUN", title, spacer)
+
 
 def main():
     random.seed(1)
@@ -128,15 +152,16 @@ def main():
         normalize_set_range(wine_set=white_normalize_range, range_dataframe=min_max_values, set_name="white")
 
         red_normalize_mean = copy.deepcopy(wine_set_red)
-        normalize_set_mean(wine_set=red_normalize_mean)
+        normalize_standard_scaler(wine_set=red_normalize_mean)
         white_normalize_mean = copy.deepcopy(wine_set_white)
-        normalize_set_mean(wine_set=white_normalize_mean)
+        normalize_standard_scaler(wine_set=white_normalize_mean)
+        #plot_hist_wine_set(red_normalize_mean, plt_figure_name="red_normalize_standard_scaler", bins=30)
 
         wine_set_list.append((red_normalize_log, "red_normalize_log"))
-        wine_set_list.append((red_normalize_range, "red_normalize_range"))
+        wine_set_list.append((red_normalize_range, "red_normalize_standard_scaler"))
         wine_set_list.append((red_normalize_mean, "red_normalize_mean"))
         wine_set_list.append((white_normalize_log, "white_normalize_log"))
-        wine_set_list.append((white_normalize_range, "white_normalize_range"))
+        wine_set_list.append((white_normalize_range, "white_normalize_standard_scaler"))
         wine_set_list.append((white_normalize_mean, "white_normalize_mean"))
 
     if DISCRETIZE:
@@ -160,7 +185,7 @@ def main():
         wine_set_list.append((reduced_set_white, "reduced_set_white"))
 
     if PLOT:
-        plot_means_red_white(wine_set_red=wine_set_red,wine_set_white=wine_set_white, )
+        plot_means_red_white(wine_set_red=wine_set_red, wine_set_white=wine_set_white, )
 
         plot_wine_set(wine_set_red, plt_figure_name="wine_set_red_plot")
         plot_wine_set(wine_set_white, plt_figure_name="wine_set_white_plot")
@@ -169,6 +194,11 @@ def main():
 
     if ALGO:
         # make a dataframe with algorithm as rows and columns as type of treatment
+        file_name = os.path.join("results.csv")
+        f = open(file_name, "w")
+        f.write("index,knn,decisionTree,mlp,agglomerative,dbscan,kmeans,\n")
+        f.close()
+
         for set in wine_set_list:
             run_algos(wine_set=set[0], title=set[1])
 
